82e42f27f6cf682b92afa99e6e6a9ae58944c115
sak <sak@firament.com>
2019-08-15 17:19:37 +0530

    Update fsap to 4.3.1

--- rn/rebuild.sh
+++ rn/rebuild.sh
@@ -22,19 +22,19 @@ npm run dist
 
 # Copy back update
 ## css
-cp -vf ${BS4_BUILD_ROOT}/dist/css/bootstrap.css ${RN_ROOT}/css/bootstrap-rn.css;
-cp -vf ${BS4_BUILD_ROOT}/dist/css/bootstrap.css.map ${RN_ROOT}/css/bootstrap-rn.css.map;
-cp -vf ${BS4_BUILD_ROOT}/dist/css/bootstrap.min.css ${RN_ROOT}/css/bootstrap-rn.min.css;
+cp -vf ${BS4_BUILD_ROOT}/dist/css/bootstrap.css         ${RN_ROOT}/css/bootstrap-rn.css;
+cp -vf ${BS4_BUILD_ROOT}/dist/css/bootstrap.css.map     ${RN_ROOT}/css/bootstrap-rn.css.map;
+cp -vf ${BS4_BUILD_ROOT}/dist/css/bootstrap.min.css     ${RN_ROOT}/css/bootstrap-rn.min.css;
 cp -vf ${BS4_BUILD_ROOT}/dist/css/bootstrap.min.css.map ${RN_ROOT}/css/bootstrap-rn.min.css.map;
 
 ## js
-cp -vf ${BS4_BUILD_ROOT}/dist/js/bootstrap.bundle.js ${RN_ROOT}/js/bootstrap-rn.bundle.js 
-cp -vf ${BS4_BUILD_ROOT}/dist/js/bootstrap.bundle.js.map ${RN_ROOT}/js/bootstrap-rn.bundle.js.map 
-cp -vf ${BS4_BUILD_ROOT}/dist/js/bootstrap.bundle.min.js ${RN_ROOT}/js/bootstrap-rn.bundle.min.js 
-cp -vf ${BS4_BUILD_ROOT}/dist/js/bootstrap.bundle.min.js.map ${RN_ROOT}/js/bootstrap-rn.bundle.min.js.map 
-cp -vf ${BS4_BUILD_ROOT}/dist/js/bootstrap.js ${RN_ROOT}/js/bootstrap-rn.js 
-cp -vf ${BS4_BUILD_ROOT}/dist/js/bootstrap.js.map ${RN_ROOT}/js/bootstrap-rn.js.map 
-cp -vf ${BS4_BUILD_ROOT}/dist/js/bootstrap.min.js ${RN_ROOT}/js/bootstrap-rn.min.js 
-cp -vf ${BS4_BUILD_ROOT}/dist/js/bootstrap.min.js.map ${RN_ROOT}/js/bootstrap-rn.min.js.map 
+cp -vf ${BS4_BUILD_ROOT}/dist/js/bootstrap.bundle.js            ${RN_ROOT}/js/bootstrap-rn.bundle.js 
+cp -vf ${BS4_BUILD_ROOT}/dist/js/bootstrap.bundle.js.map        ${RN_ROOT}/js/bootstrap-rn.bundle.js.map 
+cp -vf ${BS4_BUILD_ROOT}/dist/js/bootstrap.bundle.min.js        ${RN_ROOT}/js/bootstrap-rn.bundle.min.js 
+cp -vf ${BS4_BUILD_ROOT}/dist/js/bootstrap.bundle.min.js.map    ${RN_ROOT}/js/bootstrap-rn.bundle.min.js.map 
+cp -vf ${BS4_BUILD_ROOT}/dist/js/bootstrap.js                   ${RN_ROOT}/js/bootstrap-rn.js 
+cp -vf ${BS4_BUILD_ROOT}/dist/js/bootstrap.js.map               ${RN_ROOT}/js/bootstrap-rn.js.map 
+cp -vf ${BS4_BUILD_ROOT}/dist/js/bootstrap.min.js               ${RN_ROOT}/js/bootstrap-rn.min.js 
+cp -vf ${BS4_BUILD_ROOT}/dist/js/bootstrap.min.js.map           ${RN_ROOT}/js/bootstrap-rn.min.js.map 
 
 echo "DONE...";
